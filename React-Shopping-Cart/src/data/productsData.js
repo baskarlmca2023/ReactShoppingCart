3211bbf4e113986ffb05fbe9f7eeb25dd54fd9cc
Baskar L <139743997+baskarlmca2023@users.noreply.github.com>
2025-01-08 21:08:39 +0530

    Add files via upload

---
+++ React-Shopping-Cart/src/data/productsData.js
@@ -0,0 +1,141 @@
+// const productsData = [
+//     {
+//         id: 1,
+//         img: "/images/prod1.png",
+//         rating: "★★★★",
+//         title: "Hero GoPro 10",
+//         price: 54500,
+//         quantity: 1,
+//     },
+//     {
+//         id: 2,
+//         img: "/images/prod2.png",
+//         rating: "★★★★★",
+//         title: "Canon 1500D",
+//         price: 31000,
+//         quantity: 1,
+//     },
+//     {
+//         id: 3,
+//         img: "/images/prod3.png",
+//         rating: "★★★★",
+//         title: "Nikon Z 50",
+//         price: 84007,
+//         quantity: 1,
+//     },
+//     {
+//         id: 4,
+//         img: "/images/prod4.png",
+//         rating: "★★★★★",
+//         title: "Sony RX100",
+//         price: 53570,
+//         quantity: 1,
+//     },
+//     {
+//         id: 5,
+//         img: "/images/prod5.png",
+//         rating: "★★★★",
+//         title: "Sony Lens",
+//         price: 147990,
+//         quantity: 1,
+//     },
+//     {
+//         id: 6,
+//         img: "/images/prod6.png",
+//         rating: "★★★★★",
+//         title: "Hero GoPro 9",
+//         price: 54500,
+//         quantity: 1,
+//     },
+//     {
+//         id: 7,
+//         img: "/images/prod7.png",
+//         rating: "★★★★",
+//         title: "Canon Flash",
+//         price: 24990,
+//         quantity: 1,
+//     },
+//     {
+//         id: 8,
+//         img: "/images/prod8.png",
+//         rating: "★★★★",
+//         title: "Canon EOS",
+//         price: 54500,
+//         quantity: 1,
+//     }
+// ];
+
+// export default productsData;
+
+
+
+
+const productsData = [
+    {
+        id: 1,
+        img: "/images/HTMLCSS.jpg",
+        rating: "★★★★★",
+        title: "Responsive Web Design with HTML5 and CSS - Fourth Edition: Build future-proof responsive websites using the latest HTML5 and CSS techniques",
+        price: 1500,
+        quantity: 1,
+    },
+    {
+        id: 2,
+        img: "/images/javascript_book.webp",
+        rating: "★★★★★",
+        title: "JavaScript: The Good Parts",
+        price: 2800,
+        quantity: 1,
+    },
+
+    {
+        id: 3,
+        img: "/images/react.webp",
+        rating: "★★★★★",
+        title: "Learning React: Functional Web Development",
+        price: 3500,
+        quantity: 1,
+    },
+    {
+        id: 4,
+        img: "/images/mern.webp",
+        rating: "★★★★★",
+        title: "Full-Stack React Projects - Second Edition: Learn MERN stack development by building modern web apps using MongoDB, Express, React, and Node.js",
+        price: 4000,
+        quantity: 1,
+    },
+    {
+        id: 5,
+        img: "/images/node.webp",
+        rating: "★★★★",
+        title: "Beginning Node.js, Express & MongoDB Development",
+        price: 3800,
+        quantity: 1,
+    },
+    {
+        id: 6,
+        img: "/images/rn.webp",
+        rating: "★★★★",
+        title: "Mastering React Native (Mastering Computer Science)",
+        price: 3600,
+        quantity: 1,
+    },
+    {
+        id: 7,
+        img: "/images/git.webp",
+        rating: "★★★★",
+        title: "Beginning Git and GitHub: A Comprehensive Guide to Version Control, Project Management, and Teamwork for the New Developer",
+        price: 3400,
+        quantity: 1,
+    },
+    {
+        id: 8,
+        img: "/images/data.webp",
+        rating: "★★★★",
+        title: "Data Structures and Algorithms in C++",
+        price: 2500,
+        quantity: 1,
+    }
+];
+
+export default productsData;
